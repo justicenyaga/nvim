e57cf45345791368be041c7f9607aca98155c7db
Justice Nyaga <justiceevansmwema@gmail.com>
2025-09-15 21:03:46 +0300

    update fyler.nvim and kulala.nvim

--- lua/justice/plugins/fyler-nvim.lua
+++ lua/justice/plugins/fyler-nvim.lua
@@ -1,18 +1,25 @@
 return {
 	"A7Lavinraj/fyler.nvim",
-	dependencies = { "echasnovski/mini.icons" },
-	branch = "stable",
+	dependencies = { "echasnovski/mini.icons", "folke/snacks.nvim" },
 	opts = {
 		default_explorer = false,
 		close_on_select = false,
 		indentscope = {
 			enabled = false,
 		},
-		views = {
-			explorer = {
-				width = 0.3,
-				kind = "split:leftmost",
-			},
+		-- views = {
+		-- 	explorer = {
+		-- 		width = 0.3,
+		-- 		kind = "split_left_most",
+		-- 	},
+		-- },
+		hooks = {
+			on_rename = function(src_path, dest_path)
+				Snacks.rename.on_rename_file(src_path, dest_path)
+			end,
+		},
+		win = {
+			kind = "split_left_most",
 		},
 		mappings = {
 			explorer = {

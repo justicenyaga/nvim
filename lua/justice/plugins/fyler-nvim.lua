7da5c73fe03c6635a368af835c828441cc455694
Justice Nyaga <justiceevansmwema@gmail.com>
2025-10-13 13:33:09 +0300

    update fyler config

--- lua/justice/plugins/fyler-nvim.lua
+++ lua/justice/plugins/fyler-nvim.lua
@@ -2,17 +2,6 @@ return {
 	"A7Lavinraj/fyler.nvim",
 	dependencies = { "echasnovski/mini.icons", "folke/snacks.nvim" },
 	opts = {
-		default_explorer = false,
-		close_on_select = false,
-		indentscope = {
-			enabled = false,
-		},
-		-- views = {
-		-- 	explorer = {
-		-- 		width = 0.3,
-		-- 		kind = "split_left_most",
-		-- 	},
-		-- },
 		hooks = {
 			on_rename = function(src_path, dest_path)
 				Snacks.rename.on_rename_file(src_path, dest_path)
@@ -20,15 +9,6 @@ return {
 		},
 		win = {
 			kind = "split_left_most",
-		},
-		mappings = {
-			explorer = {
-				n = {
-					["q"] = "CloseView",
-					["<CR>"] = "Select",
-					["<C-CR>"] = "SelectRecursive",
-				},
-			},
 		},
 	},
 	keys = {

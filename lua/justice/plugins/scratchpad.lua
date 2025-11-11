0c4bae437704a191fc80675e4385ff385cce082e
Justice Nyaga <justiceevansmwema@gmail.com>
2025-11-11 11:13:31 +0300

    add width and height of scrathpad window

--- lua/justice/plugins/scratchpad.lua
+++ lua/justice/plugins/scratchpad.lua
@@ -1,7 +1,10 @@
 return {
 	"ericrswanny/chkn.nvim",
 	config = function()
-		require("chkn").setup()
+		require("chkn").setup({
+			width = 120,
+			height = 40,
+		})
 	end,
 	lazy = false,
 	keys = {

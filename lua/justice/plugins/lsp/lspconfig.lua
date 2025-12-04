3d1802d1c617ed5e7ffae45215b69e0132733744
Justice Nyaga <justiceevansmwema@gmail.com>
2025-12-04 11:51:19 +0300

    remove unnecessary lspconfig

--- lua/justice/plugins/lsp/lspconfig.lua
+++ lua/justice/plugins/lsp/lspconfig.lua
@@ -39,12 +39,6 @@ return {
 					[vim.diagnostic.severity.INFO] = "󰋼 ",
 					[vim.diagnostic.severity.HINT] = "󰌵 ",
 				},
-				numhl = {
-					[vim.diagnostic.severity.ERROR] = "",
-					[vim.diagnostic.severity.WARN] = "",
-					[vim.diagnostic.severity.HINT] = "",
-					[vim.diagnostic.severity.INFO] = "",
-				},
 			},
 		})
 
